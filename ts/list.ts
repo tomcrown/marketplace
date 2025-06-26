1ecf1c3b3607554e4e89a280a4153d4d6656578e
tomcrown <tomcrown317@gmail.com>
2025-06-26 14:05:28 +0100

    build typescript for the list function

--- ts/list.ts
+++ ts/list.ts
@@ -1,11 +1,9 @@
 import { TransactionBlock } from '@mysten/sui.js/transactions';
-import { client, keyPair } from './utils';
+import { client, keyPair, PACKAGE_ID, MODULE_NAME } from './utils';
 
-const PACKAGE_ID = '0x20b8cdb92dd81dea084c8f1122cd856b0a5485d7db97c62509ef3834bfdcbe35';
-const MODULE_NAME = 'marketplace';
-const STRUCT_NAME = 'Marketplace';
-const MARKETPLACE_ID = '0x3c8e0049398612c9db53971d746844357da77ba97a4baebeae923c173b3b8dd3';
-const ITEM_ID = '0x52bc31abf9aca41a99d8be101471e15d0534031bdd108c9a4101a2abb6fae1f7'; 
+
+const MARKETPLACE_ID = 'marketplaceId';
+const ITEM_ID = 'itemId'; 
 const LIST_PRICE = 100000000; 
 
 
@@ -19,13 +17,12 @@ async function callList() {
 			tx.object(ITEM_ID),
 			tx.pure(LIST_PRICE),
 		],
-		typeArguments: ['0x20b8cdb92dd81dea084c8f1122cd856b0a5485d7db97c62509ef3834bfdcbe35::widget::Widget', '0x2::sui::SUI']
+		typeArguments: ['$PACKAGE_ID::widget::Widget', '0x2::sui::SUI']
 	});
 
 	const result = await client.signAndExecuteTransactionBlock({
 		transactionBlock: tx,
 		signer: keyPair,
-		requestType: 'WaitForEffectsCert',
 	});
 
 	console.log('List result:', result);
