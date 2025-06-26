795797af3b1ca8c6bad2a45cc05503373571a21a
tomcrown <tomcrown317@gmail.com>
2025-06-26 13:36:48 +0100

    build typescript for the list function

--- ts/list.ts
+++ ts/list.ts
@@ -1,13 +1,12 @@
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { client, keyPair } from './utils';
 
-
-const PACKAGE_ID = '0x<your_package_id>';
+const PACKAGE_ID = '0x20b8cdb92dd81dea084c8f1122cd856b0a5485d7db97c62509ef3834bfdcbe35';
 const MODULE_NAME = 'marketplace';
 const STRUCT_NAME = 'Marketplace';
-const MARKETPLACE_ID = '0x<your_marketplace_object_id>';
-const ITEM_ID = '0x<your_nft_object_id>'; // NFT or other object to list
-const LIST_PRICE = 100000000; // adjust as needed (e.g., 1 SUI = 1_000_000_000)
+const MARKETPLACE_ID = '0x3c8e0049398612c9db53971d746844357da77ba97a4baebeae923c173b3b8dd3';
+const ITEM_ID = '0x52bc31abf9aca41a99d8be101471e15d0534031bdd108c9a4101a2abb6fae1f7'; 
+const LIST_PRICE = 100000000; 
 
 
 async function callList() {
@@ -20,12 +19,12 @@ async function callList() {
 			tx.object(ITEM_ID),
 			tx.pure(LIST_PRICE),
 		],
-		typeArguments: ['0x2::sui::SUI'], // or your custom coin type
+		typeArguments: ['0x20b8cdb92dd81dea084c8f1122cd856b0a5485d7db97c62509ef3834bfdcbe35::widget::Widget', '0x2::sui::SUI']
 	});
 
 	const result = await client.signAndExecuteTransactionBlock({
 		transactionBlock: tx,
-		signer: keypair,
+		signer: keyPair,
 		requestType: 'WaitForEffectsCert',
 	});
 
